4fa2debc3958b236d708c7615190e6233d86368a
Gregory Man <man.gregory@gmail.com>
2010-02-08 00:17:18 +0200

    CodeRunner from now run code

--- app/models/code_runner.rb
+++ app/models/code_runner.rb
@@ -1,5 +1,19 @@
-class CodeRunner < Struct.new(:file_name)
+class CodeRunner
   def perform
-    %x[java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{RAILS_ROOT}/tmp/bots/MyTronBot.rb' 'ruby #{RAILS_ROOT}/tmp/bots/MyTronBot.rb' 0 1]
+    Game.find_each(:conditions => "game_result IS NULL") {|game| 
+      result_string = run_game(game)
+      upadate_game_result(result_string, game)
+    }
+  end
+  
+  private
+  def run_game(game)
+    %x[java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{RAILS_ROOT + "/" + game.player1.code_path}/MyTronBot.rb' 'ruby #{RAILS_ROOT + "/" + game.player2.code_path}/MyTronBot.rb' 0 1]
+  end
+  
+  def upadate_game_result(result_string, game)
+    result = result_string.split("\n").last.to_i
+    game.game_result = result
+    game.save
   end
 end
--- spec/models/code_runner_spec.rb
+++ spec/models/code_runner_spec.rb
@@ -3,12 +3,46 @@ require 'spec_helper'
 describe CodeRunner do
   describe "perform" do
     before(:each) do
+      3.times do
+        Factory(:user)
+      end
+      @users = User.all
+      Tournament.create!
       @test_runner = CodeRunner.new
+      
     end
     
-    it "should run bot" do
-      @test_runner.should_receive(:'`').with("java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{RAILS_ROOT}/tmp/bots/MyTronBot.rb' 'ruby #{RAILS_ROOT}/tmp/bots/MyTronBot.rb' 0 1")
+    it "should run bots" do
+      @test_runner.stub!(:'`').and_return("1Player One Wins!\n")
+      @test_runner.should_receive(:'`').with("java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{player_code_full_path(@users[0])}' 'ruby #{player_code_full_path(@users[1])}' 0 1")
+      @test_runner.should_receive(:'`').with("java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{player_code_full_path(@users[0])}' 'ruby #{player_code_full_path(@users[2])}' 0 1")
+      @test_runner.should_receive(:'`').with("java -jar #{RAILS_ROOT}/engine/Tron.jar #{RAILS_ROOT}/maps/empty-room.txt 'ruby #{player_code_full_path(@users[1])}' 'ruby #{player_code_full_path(@users[2])}' 0 1")
       @test_runner.perform
+    end
+    
+    describe "games update" do
+      it "set 1 to game_result if player 1 win" do
+        @test_runner.stub!(:'`').and_return("1Player One Wins!\n")
+        @test_runner.perform
+        Game.last.game_result.should == 1
+      end
+      
+      it "set 2 to game_result if player 2 win" do
+        @test_runner.stub!(:'`').and_return("2Player Two Wins!\n")
+        @test_runner.perform
+        Game.last.game_result.should == 2
+      end
+      
+      it "set -1 to game_result on draw" do
+        @test_runner.stub!(:'`').and_return("-1Both players crashed. Draw!\n")
+        @test_runner.perform
+        Game.last.game_result.should == -1
+      end
+      
+    end
+    private
+    def player_code_full_path(user)
+      "#{RAILS_ROOT + "/" + user.code_path}/MyTronBot.rb"
     end
   end
 end
