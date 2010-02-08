e72d2f79692fc5b3c95aa02c75ee86877eec7042
Gregory Man <man.gregory@gmail.com>
2010-02-08 21:48:30 +0200

    Added route for GitHub Post-Receive Hook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
-  
+
+  map.github_hook 'touch-me', :controller => 'users', :action => 'update'
+    
   map.connect ':id', :controller => 'static_pages', :action => :show, :id => 'home'
   
   map.connect ':controller/:action/:id'
