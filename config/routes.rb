a5c29bf0f2a0326290538824069a6949b8384234
Aleksandr Koss <kossnocorp@gmail.com>
2010-02-09 00:01:35 +0600

    Basic static page resource was created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
   
+  map.connect ':id', :controller => 'static_pages', :action => :show, :id => 'home'
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
